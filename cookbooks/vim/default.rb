320802f077322f45e963020917ef076c784f50f2
YuyaItoh <yuya.itoh.from2012@gmail.com>
2016-11-12 00:36:08 +0900

    first commit

---
+++ cookbooks/vim/default.rb
@@ -0,0 +1,9 @@
+package "vim"
+
+## 変数設定
+dir = node[:vim][:home]
+
+## vimrcの転送
+remote_file "#{dir}/.vimrc" do
+  source "files/vimrc"
+end
---
+++ roles/base.rb
@@ -0,0 +1,8 @@
+# ********************************************
+# どのサーバでも利用する基本パッケージの構成
+# ********************************************
+
+# vim
+include_recipe "../cookbooks/vim/default.rb"
+# tmux
+include_recipe "../cookbooks/tmux/default.rb"
